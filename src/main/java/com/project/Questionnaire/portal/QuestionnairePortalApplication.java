ac3aa5afb99f5e3808bf3015b796ed7194033546
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-21 16:26:52 +0300

    init

---
+++ src/main/java/com/project/Questionnaire/portal/QuestionnairePortalApplication.java
@@ -0,0 +1,14 @@
+package com.project.Questionnaire.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+QuestionnairePortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnairePortalApplication.class, args);
+	}
+
+}
