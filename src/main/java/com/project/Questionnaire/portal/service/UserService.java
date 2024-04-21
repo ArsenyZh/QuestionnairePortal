80ed23fb49eead7926866841da2bd2234062c9a8
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-21 18:37:17 +0300

    add method: find user by email logic

--- src/main/java/com/project/Questionnaire/portal/service/UserService.java
+++ src/main/java/com/project/Questionnaire/portal/service/UserService.java
@@ -38,4 +38,10 @@ public class UserService {
 
         return user;
     }
+
+    public User findByEmail(String email) {
+        User foundUser = userRepository.findUserByEmail(email);
+
+        return foundUser;
+    }
 }
