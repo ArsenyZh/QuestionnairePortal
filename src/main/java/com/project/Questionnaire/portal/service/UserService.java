7819927202e8f6d159e99076c7abe7f40115e53d
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-21 18:06:25 +0300

    create service for user

---
+++ src/main/java/com/project/Questionnaire/portal/service/UserService.java
@@ -0,0 +1,41 @@
+package com.project.Questionnaire.portal.service;
+
+import com.project.Questionnaire.portal.entity.User;
+import com.project.Questionnaire.portal.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User registrateUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        User registeredUser = userRepository.save(user);
+
+        return registeredUser;
+    }
+
+    public User editUserInfo(User user, User updatedUser) {
+        user.setFirstName(updatedUser.getFirstName());
+        user.setLastName(updatedUser.getLastName());
+        user.setEmail(updatedUser.getEmail());
+        user.setPhoneNumber(updatedUser.getPhoneNumber());
+        userRepository.save(user);
+
+        return user;
+    }
+
+    public User changeUserPassword(User user, User updatedUser) {
+        user.setPassword(updatedUser.getPassword());
+        userRepository.save(user);
+
+        return user;
+    }
+}
