30656b4cd8c5ce1b8c5f530f4bb24cbf943bdc7f
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-22 10:44:39 +0300

    create config for security logic

--- src/main/java/com/project/Questionnaire/portal/config/SecurityConfig.java
+++ src/main/java/com/project/Questionnaire/portal/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.project.Questionnaire.portal.config;
 
+import com.project.Questionnaire.portal.service.CustomUserDetailService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @AllArgsConstructor
@@ -14,5 +18,21 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/sign_up", "/log_in", "/swagger-ui/index.html#/").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
     }
 }
