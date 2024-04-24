5f9612b49dbec69a3ea1d368a1d8eee4c60cb06a
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-24 20:33:38 +0300

    create controller for entity field

---
+++ src/main/java/com/project/Questionnaire/portal/controller/FieldController.java
@@ -0,0 +1,51 @@
+package com.project.Questionnaire.portal.controller;
+
+import com.project.Questionnaire.portal.dto.FieldDto;
+import com.project.Questionnaire.portal.entity.Field;
+import com.project.Questionnaire.portal.mapper.FieldMapper;
+import com.project.Questionnaire.portal.service.FieldService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class FieldController {
+    @Autowired
+    private FieldService fieldService;
+    @Autowired
+    private FieldMapper fieldMapper;
+
+    @GetMapping("/fields")
+    public List<FieldDto> getFields() {
+        List<Field> fieldList = fieldService.findAllFields();
+        List<FieldDto> fieldDtoList = fieldMapper.toFieldDto(fieldList);
+
+        return fieldDtoList;
+    }
+
+    @PostMapping("/fields/create")
+    public FieldDto createField(@RequestBody FieldDto fieldDto) {
+        Field field = fieldService.createField(fieldMapper.toField(fieldDto));
+
+        return fieldMapper.toFieldDto(field);
+    }
+
+    @PutMapping("/fields/update/{id}")
+    public FieldDto updateField(@RequestBody FieldDto fieldDto, @PathVariable("id") Long fieldId) {
+        Field field = fieldService.findById(fieldId);
+
+        if (field != null) {
+            return fieldMapper.toFieldDto(field);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/fields/delete/{id}")
+    public void deleteField(@PathVariable("id") Long fieldId) {
+        fieldService.deleteField(fieldId);
+    }
+}
