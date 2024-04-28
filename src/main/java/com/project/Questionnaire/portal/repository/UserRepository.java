1a537b7cd477d7b9260f2a46a96a449cd2e3526f
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-04-28 17:22:41 +0300

    remove method

--- src/main/java/com/project/Questionnaire/portal/repository/UserRepository.java
+++ src/main/java/com/project/Questionnaire/portal/repository/UserRepository.java
@@ -8,6 +8,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findUserByEmail(String email);
-    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE email = :login")
-    User getUserByLogin(String login);
 }
