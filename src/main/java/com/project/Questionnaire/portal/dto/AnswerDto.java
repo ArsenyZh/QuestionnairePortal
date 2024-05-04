6c792b91c681e73c590d6f26c5da4708f1c6cb45
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-04 17:48:01 +0300

    create dto for answer entity

---
+++ src/main/java/com/project/Questionnaire/portal/dto/AnswerDto.java
@@ -0,0 +1,11 @@
+package com.project.Questionnaire.portal.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AnswerDto {
+    private String answer;
+    private Long fieldId;
+}
