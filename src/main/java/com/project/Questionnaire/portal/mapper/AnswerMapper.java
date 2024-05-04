cfcefbc87a842cd11cc72b5c9a504f285cc381ef
Arseni Zhirnevich <szhirnevich@mail.ru>
2024-05-04 17:48:12 +0300

    create mapper for answer entity

---
+++ src/main/java/com/project/Questionnaire/portal/mapper/AnswerMapper.java
@@ -0,0 +1,25 @@
+package com.project.Questionnaire.portal.mapper;
+
+import com.project.Questionnaire.portal.dto.AnswerDto;
+import com.project.Questionnaire.portal.entity.Answer;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class AnswerMapper {
+
+    public List<AnswerDto> toAnswerDto(List<Answer> answerList) {
+        List<AnswerDto> answerDtoList = new ArrayList<>();
+
+        for (Answer answer : answerList) {
+            AnswerDto answerDto = new AnswerDto();
+            answerDto.setAnswer(answer.getAnswer());
+            answerDto.setFieldId(answer.getField().getId());
+            answerDtoList.add(answerDto);
+        }
+
+        return answerDtoList;
+    }
+}
